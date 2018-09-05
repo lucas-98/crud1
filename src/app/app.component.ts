25fab8ac7ce76d0e35f590b067d7df98d1be079a
lucas carranza <lucas.carranza98@gmail>
2018-09-05 09:08:49 -0300

    'subiendo'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Empleado } from './models/empleado';
+ 
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud1';
+  empleadoArray: Empleado[] =[
+    {id: 1, nombre: 'Gabriel', pais:'Brasil'},
+    {id: 2, nombre: 'Fernanda', pais:'Brasil'},
+    {id: 3, nombre: 'Emanuel', pais:'Brasil'},
+    {id: 4, nombre: 'Jose', pais:'Brasil'}
+  ];
+
+  seleccionarUnEmpleado : Empleado = new Empleado();
+
+  addOrEdit(){
+    if(this.seleccionarUnEmpleado.id === 0){
+      this.seleccionarUnEmpleado.id = this.empleadoArray.length +1;
+      this.empleadoArray.push(this.seleccionarUnEmpleado);
+    } 
+
+    this.seleccionarUnEmpleado = new Empleado();
+
+  }
+
+  abrirParaEditar(empleado: Empleado){
+    this.seleccionarUnEmpleado = empleado;
+
+  }
+
+  eliminarEmpleado(){
+    if(confirm('Estas seguro de querer eliminarlo?')) {
+      this.empleadoArray = this.empleadoArray.filter(x => x != this.seleccionarUnEmpleado )
+      this.seleccionarUnEmpleado = new Empleado();
+    }
+  }
+
 }
+
